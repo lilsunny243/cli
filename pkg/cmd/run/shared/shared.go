d61eb0219b8fd1467ca4b2642d8f35469ee06a15
BakedSunny <110278648+lilsunny243@users.noreply.github.com>
2022-09-15 18:14:04 +0200

    Squashed commit of the following:
    commit 50c3fbc9ae2e112c86cf18b25217250a331760f5
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:49:34 2021 -0800
    
        fix failing test
    
    commit 847a32aa41b35dbad44d6363c98dfeb09e2ca4b0
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:46:06 2021 -0800
    
        add failing test
    
    commit da594216ddbeaa26bdbe50030c76e169143bdccd
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:45:36 2021 -0800
    
        linter appeasement
    
    commit 2a1c601244c8f57d66b6529a33a7d20510261e84
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:40:41 2021 -0800
    
        reintroduce linter error
    
    commit 321f99b5a662ad0dd8edde2aa8641456f4166949
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:37:39 2021 -0800
    
        fix broken demo test
    
    commit 9f7761223b9e149aa47d1fa34deccfe5f053679b
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:33:32 2021 -0800
    
        add new test
    
    commit 399fc4ae9cd6cc8a5ef11556f94438bb8bf304d6
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:32:56 2021 -0800
    
        make linter happy
    
    commit 3009ec2a2153387bf4e8836ac9716f39af4ba1f7
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:23:37 2021 -0800
    
        add demo test
    
    commit be9c2ce1275d593ac8a63fd1dd8f574d0863ca24
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:18:11 2021 -0800
    
        fix tests
    
    commit 5da9a7837c40f1dc7100c1e99e042522bdd7ac90
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:08:10 2021 -0800
    
        auto select job if only one in job view
    
    commit 6999148168e4b52c66be2c17b62880d82a4b568f
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:07:01 2021 -0800
    
        show commit msg when selecting runs
    
    commit 011c13413eba30b8bf67d393210e7b69aeef34c8
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 14:00:25 2021 -0800
    
        linter appeasement
    
    commit 97ff5e0c12756f7d6474c7f21657a4778a52cb3d
    Author: vilmibm <vilmibm@github.com>
    Date:   Fri Feb 5 13:16:29 2021 -0800
    
        experiment with columns in run list
    
    commit b34efd38e063b58ddc6af258c6a7479f7eccec0d
    Author: vilmibm <vilmibm@github.com>
    Date:   Wed Jan 27 17:05:49 2021 -0800
    
        Implement support for GitHub Actions
    
        - add Cyanf and Grayf helpers
        - add gh actions
        - add gh run list
          - support base repo
          - grab runs
          - paginate
          - begin formatting
          - basic arg tests
          - nontty output
          - show elapsed
        - split new types into run/shared
        - add gh run view
          - basic arg tests
          - progress indicator
          - interactive selector
          - rendering a run
            - job summary
            - switch from templates to linewise printing
            - use table printer for jobs
            - fuzzy ago
            - annotations
        - add gh job view
          - interactive selector
          - step rendering
          - annotation rendering
          - share annotation code
          - log rendering (--log)
        - add next step hints
        - tweak formatting
        - support run view -v

--- pkg/cmd/run/shared/shared.go
+++ pkg/cmd/run/shared/shared.go
@@ -1,106 +1,59 @@
 package shared
 
 import (
-	"archive/zip"
-	"errors"
 	"fmt"
 	"net/url"
-	"reflect"
 	"strings"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
-	"github.com/cli/cli/v2/api"
-	"github.com/cli/cli/v2/internal/ghrepo"
-	"github.com/cli/cli/v2/pkg/iostreams"
-	"github.com/cli/cli/v2/pkg/prompt"
+	"github.com/cli/cli/api"
+	"github.com/cli/cli/internal/ghrepo"
+	"github.com/cli/cli/pkg/iostreams"
+	"github.com/cli/cli/pkg/prompt"
 )
 
 const (
 	// Run statuses
-	Queued     Status = "queued"
-	Completed  Status = "completed"
-	InProgress Status = "in_progress"
-	Requested  Status = "requested"
-	Waiting    Status = "waiting"
+	Queued     = "queued"
+	Completed  = "completed"
+	InProgress = "in_progress"
+	Requested  = "requested"
+	Waiting    = "waiting"
 
 	// Run conclusions
-	ActionRequired Conclusion = "action_required"
-	Cancelled      Conclusion = "cancelled"
-	Failure        Conclusion = "failure"
-	Neutral        Conclusion = "neutral"
-	Skipped        Conclusion = "skipped"
-	Stale          Conclusion = "stale"
-	StartupFailure Conclusion = "startup_failure"
-	Success        Conclusion = "success"
-	TimedOut       Conclusion = "timed_out"
+	ActionRequired = "action_required"
+	Cancelled      = "cancelled"
+	Failure        = "failure"
+	Neutral        = "neutral"
+	Skipped        = "skipped"
+	Stale          = "stale"
+	StartupFailure = "startup_failure"
+	Success        = "success"
+	TimedOut       = "timed_out"
 
-	AnnotationFailure Level = "failure"
-	AnnotationWarning Level = "warning"
+	// TODO events
 )
+
+// TODO URL printing/-w
+// TODO various nontty support
 
 type Status string
 type Conclusion string
-type Level string
-
-var RunFields = []string{
-	"name",
-	"headBranch",
-	"headSha",
-	"createdAt",
-	"updatedAt",
-	"startedAt",
-	"status",
-	"conclusion",
-	"event",
-	"databaseId",
-	"workflowDatabaseId",
-	"url",
-}
+type RunEvent string
 
 type Run struct {
-	Name           string
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
-	StartedAt      time.Time `json:"run_started_at"`
-	Status         Status
-	Conclusion     Conclusion
-	Event          string
-	ID             int64
-	WorkflowID     int64  `json:"workflow_id"`
-	Attempts       uint8  `json:"run_attempt"`
-	HeadBranch     string `json:"head_branch"`
-	JobsURL        string `json:"jobs_url"`
-	HeadCommit     Commit `json:"head_commit"`
-	HeadSha        string `json:"head_sha"`
-	URL            string `json:"html_url"`
-	HeadRepository Repo   `json:"head_repository"`
-}
-
-func (r *Run) StartedTime() time.Time {
-	if r.StartedAt.IsZero() {
-		return r.CreatedAt
-	}
-	return r.StartedAt
-}
-
-func (r *Run) Duration(now time.Time) time.Duration {
-	endTime := r.UpdatedAt
-	if r.Status != Completed {
-		endTime = now
-	}
-	d := endTime.Sub(r.StartedTime())
-	if d < 0 {
-		return 0
-	}
-	return d.Round(time.Second)
-}
-
-type Repo struct {
-	Owner struct {
-		Login string
-	}
-	Name string
+	Name       string
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	Status     Status
+	Conclusion Conclusion
+	Event      RunEvent
+	ID         int
+	HeadBranch string `json:"head_branch"`
+	JobsURL    string `json:"jobs_url"`
+	HeadCommit Commit `json:"head_commit"`
+	HeadSha    string `json:"head_sha"`
 }
 
 type Commit struct {
@@ -116,40 +69,14 @@ func (r Run) CommitMsg() string {
 	}
 }
 
-func (r *Run) ExportData(fields []string) map[string]interface{} {
-	v := reflect.ValueOf(r).Elem()
-	fieldByName := func(v reflect.Value, field string) reflect.Value {
-		return v.FieldByNameFunc(func(s string) bool {
-			return strings.EqualFold(field, s)
-		})
-	}
-	data := map[string]interface{}{}
-
-	for _, f := range fields {
-		switch f {
-		case "databaseId":
-			data[f] = r.ID
-		case "workflowDatabaseId":
-			data[f] = r.WorkflowID
-		default:
-			sf := fieldByName(v, f)
-			data[f] = sf.Interface()
-		}
-	}
-
-	return data
-}
-
 type Job struct {
-	ID          int64
+	ID          int
 	Status      Status
 	Conclusion  Conclusion
 	Name        string
-	Steps       Steps
+	Steps       []Step
 	StartedAt   time.Time `json:"started_at"`
 	CompletedAt time.Time `json:"completed_at"`
-	URL         string    `json:"html_url"`
-	RunID       int64     `json:"run_id"`
 }
 
 type Step struct {
@@ -157,36 +84,30 @@ type Step struct {
 	Status     Status
 	Conclusion Conclusion
 	Number     int
-	Log        *zip.File
 }
-
-type Steps []Step
-
-func (s Steps) Len() int           { return len(s) }
-func (s Steps) Less(i, j int) bool { return s[i].Number < s[j].Number }
-func (s Steps) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 type Annotation struct {
 	JobName   string
 	Message   string
 	Path      string
-	Level     Level `json:"annotation_level"`
-	StartLine int   `json:"start_line"`
+	Level     string `json:"annotation_level"`
+	StartLine int    `json:"start_line"`
 }
 
-func AnnotationSymbol(cs *iostreams.ColorScheme, a Annotation) string {
+func (a Annotation) Symbol(cs *iostreams.ColorScheme) string {
+	// TODO types for levels
 	switch a.Level {
-	case AnnotationFailure:
+	case "failure":
 		return cs.FailureIcon()
-	case AnnotationWarning:
-		return cs.WarningIcon()
+	case "warning":
+		return cs.Yellow("!")
 	default:
-		return "-"
+		return "TODO"
 	}
 }
 
 type CheckRun struct {
-	ID int64
+	ID int
 }
 
 func GetAnnotations(client *api.Client, repo ghrepo.Interface, job Job) ([]Annotation, error) {
@@ -196,10 +117,6 @@ func GetAnnotations(client *api.Client, repo ghrepo.Interface, job Job) ([]Annot
 
 	err := client.REST(repo.RepoHost(), "GET", path, nil, &result)
 	if err != nil {
-		var httpError api.HTTPError
-		if errors.As(err, &httpError) && httpError.StatusCode == 404 {
-			return []Annotation{}, nil
-		}
 		return nil, err
 	}
 
@@ -227,41 +144,7 @@ type RunsPayload struct {
 	WorkflowRuns []Run `json:"workflow_runs"`
 }
 
-type FilterOptions struct {
-	Branch string
-	Actor  string
-}
-
-func GetRunsWithFilter(client *api.Client, repo ghrepo.Interface, opts *FilterOptions, limit int, f func(Run) bool) ([]Run, error) {
-	path := fmt.Sprintf("repos/%s/actions/runs", ghrepo.FullName(repo))
-	runs, err := getRuns(client, repo, path, opts, 50)
-	if err != nil {
-		return nil, err
-	}
-	filtered := []Run{}
-	for _, run := range runs {
-		if f(run) {
-			filtered = append(filtered, run)
-		}
-		if len(filtered) == limit {
-			break
-		}
-	}
-
-	return filtered, nil
-}
-
-func GetRunsByWorkflow(client *api.Client, repo ghrepo.Interface, opts *FilterOptions, limit int, workflowID int64) ([]Run, error) {
-	path := fmt.Sprintf("repos/%s/actions/workflows/%d/runs", ghrepo.FullName(repo), workflowID)
-	return getRuns(client, repo, path, opts, limit)
-}
-
-func GetRuns(client *api.Client, repo ghrepo.Interface, opts *FilterOptions, limit int) ([]Run, error) {
-	path := fmt.Sprintf("repos/%s/actions/runs", ghrepo.FullName(repo))
-	return getRuns(client, repo, path, opts, limit)
-}
-
-func getRuns(client *api.Client, repo ghrepo.Interface, path string, opts *FilterOptions, limit int) ([]Run, error) {
+func GetRuns(client *api.Client, repo ghrepo.Interface, limit int) ([]Run, error) {
 	perPage := limit
 	page := 1
 	if limit > 100 {
@@ -273,26 +156,11 @@ func getRuns(client *api.Client, repo ghrepo.Interface, path string, opts *Filte
 	for len(runs) < limit {
 		var result RunsPayload
 
-		parsed, err := url.Parse(path)
-		if err != nil {
-			return nil, err
-		}
-		query := parsed.Query()
-		query.Set("per_page", fmt.Sprintf("%d", perPage))
-		query.Set("page", fmt.Sprintf("%d", page))
-		if opts != nil {
-			if opts.Branch != "" {
-				query.Set("branch", opts.Branch)
-			}
-			if opts.Actor != "" {
-				query.Set("actor", opts.Actor)
-			}
-		}
-		parsed.RawQuery = query.Encode()
-		pagedPath := parsed.String()
+		path := fmt.Sprintf("repos/%s/actions/runs?per_page=%d&page=%d", ghrepo.FullName(repo), perPage, page)
 
-		err = client.REST(repo.RepoHost(), "GET", pagedPath, nil, &result)
+		err := client.REST(repo.RepoHost(), "GET", path, nil, &result)
 		if err != nil {
+			// TODO better err handle
 			return nil, err
 		}
 
@@ -306,11 +174,6 @@ func getRuns(client *api.Client, repo ghrepo.Interface, path string, opts *Filte
 				break
 			}
 		}
-
-		if len(result.WorkflowRuns) < perPage {
-			break
-		}
-
 		page++
 	}
 
@@ -323,41 +186,39 @@ type JobsPayload struct {
 
 func GetJobs(client *api.Client, repo ghrepo.Interface, run Run) ([]Job, error) {
 	var result JobsPayload
-	if err := client.REST(repo.RepoHost(), "GET", run.JobsURL, nil, &result); err != nil {
+	parsed, err := url.Parse(run.JobsURL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.REST(repo.RepoHost(), "GET", parsed.Path[1:], nil, &result)
+	if err != nil {
 		return nil, err
 	}
 	return result.Jobs, nil
 }
 
-func GetJob(client *api.Client, repo ghrepo.Interface, jobID string) (*Job, error) {
-	path := fmt.Sprintf("repos/%s/actions/jobs/%s", ghrepo.FullName(repo), jobID)
-
-	var result Job
-	err := client.REST(repo.RepoHost(), "GET", path, nil, &result)
+func PromptForRun(cs *iostreams.ColorScheme, client *api.Client, repo ghrepo.Interface) (string, error) {
+	// TODO arbitrary limit
+	runs, err := GetRuns(client, repo, 10)
 	if err != nil {
-		return nil, err
+		// TODO better error handle
+		return "", err
 	}
 
-	return &result, nil
-}
-
-func PromptForRun(cs *iostreams.ColorScheme, runs []Run) (string, error) {
 	var selected int
-	now := time.Now()
 
 	candidates := []string{}
 
 	for _, run := range runs {
-		symbol, _ := Symbol(cs, run.Status, run.Conclusion)
+		symbol := Symbol(cs, run.Status, run.Conclusion)
 		candidates = append(candidates,
-			// TODO truncate commit message, long ones look terrible
-			fmt.Sprintf("%s %s, %s (%s) %s", symbol, run.CommitMsg(), run.Name, run.HeadBranch, preciseAgo(now, run.StartedTime())))
+			fmt.Sprintf("%s %s, %s (%s)", symbol, run.CommitMsg(), run.Name, run.HeadBranch))
 	}
 
 	// TODO consider custom filter so it's fuzzier. right now matches start anywhere in string but
 	// become contiguous
-	//nolint:staticcheck // SA1019: prompt.SurveyAskOne is deprecated: use Prompter
-	err := prompt.SurveyAskOne(&survey.Select{
+	err = prompt.SurveyAskOne(&survey.Select{
 		Message:  "Select a workflow run",
 		Options:  candidates,
 		PageSize: 10,
@@ -383,99 +244,17 @@ func GetRun(client *api.Client, repo ghrepo.Interface, runID string) (*Run, erro
 	return &result, nil
 }
 
-type colorFunc func(string) string
-
-func Symbol(cs *iostreams.ColorScheme, status Status, conclusion Conclusion) (string, colorFunc) {
-	noColor := func(s string) string { return s }
+func Symbol(cs *iostreams.ColorScheme, status Status, conclusion Conclusion) string {
 	if status == Completed {
 		switch conclusion {
 		case Success:
-			return cs.SuccessIconWithColor(noColor), cs.Green
-		case Skipped, Neutral:
-			return "-", cs.Gray
+			return cs.SuccessIcon()
+		case Skipped, Cancelled, Neutral:
+			return cs.SuccessIconWithColor(cs.Gray)
 		default:
-			return cs.FailureIconWithColor(noColor), cs.Red
+			return cs.FailureIcon()
 		}
 	}
 
-	return "*", cs.Yellow
-}
-
-func PullRequestForRun(client *api.Client, repo ghrepo.Interface, run Run) (int, error) {
-	type response struct {
-		Repository struct {
-			PullRequests struct {
-				Nodes []struct {
-					Number         int
-					HeadRepository struct {
-						Owner struct {
-							Login string
-						}
-						Name string
-					}
-				}
-			}
-		}
-		Number int
-	}
-
-	variables := map[string]interface{}{
-		"owner":       repo.RepoOwner(),
-		"repo":        repo.RepoName(),
-		"headRefName": run.HeadBranch,
-	}
-
-	query := `
-		query PullRequestForRun($owner: String!, $repo: String!, $headRefName: String!) {
-			repository(owner: $owner, name: $repo) {
-				pullRequests(headRefName: $headRefName, first: 1, orderBy: { field: CREATED_AT, direction: DESC }) {
-					nodes {
-						number
-						headRepository {
-							owner {
-								login
-							}
-							name
-						}
-					}
-				}
-			}
-		}`
-
-	var resp response
-
-	err := client.GraphQL(repo.RepoHost(), query, variables, &resp)
-	if err != nil {
-		return -1, err
-	}
-
-	prs := resp.Repository.PullRequests.Nodes
-	if len(prs) == 0 {
-		return -1, fmt.Errorf("no matching PR found for %s", run.HeadBranch)
-	}
-
-	number := -1
-
-	for _, pr := range prs {
-		if pr.HeadRepository.Owner.Login == run.HeadRepository.Owner.Login && pr.HeadRepository.Name == run.HeadRepository.Name {
-			number = pr.Number
-		}
-	}
-
-	if number == -1 {
-		return number, fmt.Errorf("no matching PR found for %s", run.HeadBranch)
-	}
-
-	return number, nil
-}
-
-func preciseAgo(now time.Time, createdAt time.Time) string {
-	ago := now.Sub(createdAt)
-
-	if ago < 30*24*time.Hour {
-		s := ago.Truncate(time.Second).String()
-		return fmt.Sprintf("%s ago", s)
-	}
-
-	return createdAt.Format("Jan _2, 2006")
+	return cs.Yellow("-")
 }
